fae8e7ea392e4c11f008a8cd62dd70d802341894
Max Czapanskiy <47009796+FlukeAndFeather@users.noreply.github.com>
2025-01-21 08:36:26 -0800

    prog102 updates

--- prog102.R
+++ prog102.R
@@ -1,33 +1,54 @@
 library(marinecs100b)
 
-# Extracting data ---------------------------------------------------------
 
-# How did you extract the temperature and exposure from the hottest day?
+# Writing a utility function ----------------------------------------------
+
+# P1: How did you extract the temperature and exposure from the hottest day?
 # Copy-paste the code here.
 
-# If you want to change the site and date, what variables would need to change?
+# P2: Fill in the blanks below to write the Alaskan datetime utility function.
+??? <- ???(???) {
+  ???
+  return(???)
+}
 
-# What would you pick for the temperature extraction function name and parameter
-# names?
+# Extracting data ---------------------------------------------------------
 
+# P3: Make a copy of your code from P1 and edit it to plot the temperature and
+# exposure for "Aialik" on 2012-06-01
+
+# P4: Make a copy of your code from P3 and edit it to plot the temperature and
+# exposure for "Harris" on 2016-04-05.
+
+# P5: Compare your solutions for P3 and P4 - what variables changed?
+
+# P6: What you would pick for the temperature extraction function and
+# parameters' names?
 
 # Writing extraction functions --------------------------------------------
 
-# Write your temperature extraction function here
+# P7: Fill in the blanks in the code below to write your temperature extraction
+# function.
 
-# Write your exposure extraction function here
+??? <- ???(???, ???, ???) {
+  ??? <- your_utility_function_name_here(???)
+  ??? <- your_utility_function_name_here(???)
+  ???
+  return(???)
+}
 
-# Write your datetime extraction function here
+# P8: Make a copy of your solution to P7, and edit it to create exposure and
+# datetime extraction functions.
 
-# Visualize Nuka Pass temperatures on 2018-07-01
+# P9: Export your annotated screenshot as a JPEG called "annotated_function.jpg"
+# and add it to your copy of the module repository. (It should be in the same
+# folder as this file.)
 
-# How does the visualization code above compare to what you wrote in PROG101?
+# P10: Visualize Nuka Pass on July 1, 2018.
 
+# P11: Save a copy of the Nuka Pass plot as "nuka_pass_2018-07-01.png" in this
+# repo
 
-# Functions within functions (optional) -----------------------------------
-
-# How would you parameterize the visualization code into its own function?
-# Specifically, what would you call that function and its parameters?
-
-# Write your wrapper function for plot_kefj(). Call it to compare the
-# visualizations for Nuka Pass on July 1, 2018 versus January 1, 2018.
+# P12: Compare the code you wrote to create the plot in this module to the code
+# you wrote in PROG101. Qualitatively, how do they compare? Which one is easier
+# to read and why?
